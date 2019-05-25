99f48300f82751dd56f0a6f6e9ba4889c074b76d
Vigneshwaran Kannan <vignesh.mountblue@embibe.com>
2019-05-26 00:57:41 +0530

    add online offlfine capablity

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import MainPage from './modules/MainPage/index';
+import { Offline, Online } from "react-detect-offline";
 
-function App() {
-  return (
-    <MainPage />
-  );
-}
+const App = () => (
+  <div>
+    <Online><MainPage/></Online>
+    <Offline>
+      <div className='offline-container'>
+        <span>Please check your internet connectivity!</span>
+        <span>Please be Online to enjoy this app...</span>
+      </div>
+    </Offline>
+  </div>
+);
 
 export default App;
